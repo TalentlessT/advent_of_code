cd620ac97b926561ed971d1a05827c659291adc4
TalentlessT <jan.horak343@gmail.com>
2021-12-15 20:42:42 +0100

    first task - 2/2

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -10,11 +10,21 @@ fn main() {
 
     let content = fs::read_to_string(filename).expect("Something went wrong.");
 
-    let num_array: Vec<i32> = content
+    let num_array_1: Vec<i32> = content
         .split_whitespace()
         .map(|s| s.parse().expect("parse error"))
         .collect();
 
+    let num_array_2: Vec<i32> = content
+        .split_whitespace()
+        .map(|s| s.parse().expect("parse error"))
+        .collect();
+
+    first_half(num_array_1);
+    second_half(num_array_2);
+}
+
+fn first_half(num_array: Vec<i32>) {
     let mut x: usize = 0;
     let mut y: usize = 1;
     let mut result = 0;
@@ -34,5 +44,30 @@ fn main() {
         y += 1;
     }
 
-    println!("result: {}", result);
+    println!("first result: {}", result);
+}
+
+fn second_half(num_array: Vec<i32>) {
+    let mut x: usize = 0;
+    let mut y: usize = 1;
+    let mut result = 0;
+
+    let count: i32 = num_array.len().try_into().unwrap();
+
+    loop {
+        if x > (count - 4).try_into().unwrap() {
+            println!("exiting...");
+            break;
+        }
+
+        let sum1: i32 = num_array[x] + num_array[x + 1] + num_array[x + 2];
+        let sum2: i32 = num_array[y] + num_array[y + 1] + num_array[y + 2];
+
+        if sum1 < sum2 {
+            result += 1;
+        }
+        x += 1;
+        y += 1;
+    }
+    println!("second result: {}", result);
 }
